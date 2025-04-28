c7e20ee4601c5bb5e65c177b2aa563a15782e540
Kenkods <jkennn02@gmail.com>
2025-04-29 00:44:23 +0800

    Front

---
+++ Thunderkick-advdb/api/api.php
@@ -0,0 +1,34 @@
+<?php
+     header('Content-Type: application/json');
+
+     $products=[
+        [
+            "name"=>"Adizero EVO SL Shoes",
+            "brand"=>"Adidas",
+            "price"=>"₱8,000",
+            "type"=>"Running",
+            "catergory"=>"mens"  
+        ],
+        [
+            "name"=>"Harden Volume 9 Shoes",
+            "brand"=>"Adidas",
+            "price"=>"₱9,500",
+            "type"=>"Basketball",
+            "category"=>"mens"
+        ],
+        [
+            "name"=>"Samba OG Shoes",
+            "brand"=>"Adidas",
+            "price"=>"6,800",
+            "type"=>"casual",
+            "catergory"=>"Uni"
+
+        ],
+        [
+        "name"=>"Anthony Edwards 1 Low Trainer",
+        "brand"=>"Adidas",
+        "price"=>"₱5,800",
+        "type"=>"Basketball",
+        "catergory"=>"kids"
+        ]
+        ];
