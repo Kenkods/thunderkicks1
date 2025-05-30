1eed3c05e2712b55c908931d5a1b239a3a7d6372
Kenkods <jkennn02@gmail.com>
2025-05-30 16:34:02 +0800

    D

--- Thunderkick-advdb/pages/Landing/Landing.php
+++ Thunderkick-advdb/pages/Landing/Landing.php
@@ -131,28 +131,25 @@
 
 
       <div class="card bg-white rounded-xl shadow-md overflow-hidden  hover:drop-shadow-[0px_4px_5px_rgba(77,77,92,0.8)] transition-shadow duration-300 border-1 border-gray-200  my-3 mx-4 max-w-65 hover:cursor-pointer">
-        <input name="shoe_id" id="" type="hidden" value=<?= htmlspecialchars($card['shoe_id'])?>>
-      <img class=" h-40 w-full object-cover" src=<?=htmlspecialchars($card['shoe_img'])?> alt="Product Image">
-        <input type="hidden" value="<?=htmlspecialchars($card['shoe_id'])?>">
+        <input name="shoe_id" id="" type="hidden" value=<?= htmlspecialchars($cards['shoe_id'])?>>
+      <img class=" h-40 w-full object-cover" src=<?=htmlspecialchars($cards['shoe_img'])?> alt="Product Image">
+        <input type="hidden" value="<?=htmlspecialchars($cards['shoe_id'])?>">
         <div class="px-3 py-4">
           <div class=" flex flex-wrap  h-15">
-          <h2 class="text-xl font-semibold font-mono text-gray-800 flex flex-wrap w-60"><?=htmlspecialchars($card['name'])?></h2>
+          <h2 class="text-xl font-semibold font-mono text-gray-800 flex flex-wrap w-60"><?=htmlspecialchars($cards['name'])?></h2>
           </div>
           <div class="flex flex-wrap px-2">
-          <?php if (!empty($card['sizes']) && is_array($card['sizes'])): ?>
-            <?php foreach ($card['sizes'] as $size): ?>
-              <button  type="button" class="size-btn hover:scale-110 border-2 px-2  hover:cursor-pointer  transform transition duration-150 mx-1"  data-size="<?=htmlspecialchars($size['size'])?>"><?=htmlspecialchars($size['size'])?></button>
-              <?php endforeach; ?>
-            <?php endif;?>
-            </div>
-          <div class="mt-4 flex justify-between items-center">
-            <span class=" font-bold text-lg">$<?=htmlspecialchars($card['price'])?></span>
+              
+         
+       
+
+        
+
            
              <?php if(isset($_SESSION['user'])): ?>
 
               <!-- <form method="POST" action="/Thunderkicks1/Thunderkick-advdb/public/index.php?page=addToCart"> -->
                 
-                <button  id="addCartBtn" class="addCartBtn px-4 py-2 bg-yellow-600 hover:bg-yellow-500 text-white font-bold rounded-xl hover:cursor-pointer">Add to Cart</button>
               <!-- </form> -->
             <?php else: ?>
               <a href="/Thunderkicks1/Thunderkick-advdb/public/login" class="px-4 py-2 bg-yellow-600 hover:bg-yellow-500 text-white font-bold rounded-xl hover:cursor-pointer">Add to Cart</a>
@@ -162,7 +159,7 @@
         </div>
       </div>
         <?php endforeach;?>
-       <button class=" border-1 relative top-5 px-5 py-2 font-medium hover:cursor-pointer hover:scale-105 transition duration-300"> <a href="/Thunderkicks1/Thunderkick-advdb/public/products">View All</a></button>
+       <button class=" border-1 relative top-5 px-5 py-2 font-medium hover:cursor-pointer hover:scale-105 transition duration-300"> <a href="/Thunderkicks1/Thunderkick-advdb/public/products">Go To Products</a></button>
 
       </div>
 
